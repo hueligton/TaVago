6ae6a7f6066cbbb54cc5b5309c02a2a6e9e6ed9a
hueligton <hueligton@outlook.com>
2018-11-17 14:36:16 -0300

    Unificação de sql de inserção
    Unificação dos arquivos de inserção feito pela @vitoriaholanda08 e pelo @guiganafe

--- src/sql/Reserva.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO RESERVA VALUES (1, 1), ##Reserva em nome de Patricia Campos
-INSERT INTO RESERVA VALUES (2, 2), ##Reserva em nome de Joao Almeida
-INSERT INTO RESERVA VALUES (3, 3); ##Reserva em nome de Ana Maria 
